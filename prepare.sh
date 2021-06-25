29f5a452f2cd163c54c8b6a2b4629fe6d5b92fa6
Robert Luke <748691+rob-luke@users.noreply.github.com>
2021-06-25 20:36:26 +1000

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+
+# We start by adding extra apt packages, since pip modules may required library
+if [ "$EXTRA_APT_PACKAGES" ]; then
+    echo "EXTRA_APT_PACKAGES environment variable found.  Installing."
+    apt update -y
+    apt install -y $EXTRA_APT_PACKAGES
+fi
+
+if [ -e "/opt/app/environment.yml" ]; then
+    echo "environment.yml found. Installing packages"
+    /opt/conda/bin/conda env update -f /opt/app/environment.yml
+else
+    echo "no environment.yml"
+fi
+
+if [ "$EXTRA_CONDA_PACKAGES" ]; then
+    echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
+    /opt/conda/bin/conda install -y $EXTRA_CONDA_PACKAGES
+fi
+
+if [ "$EXTRA_PIP_PACKAGES" ]; then
+    echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
+    /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
+fi
+
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1400x900x24 -ac +extension GLX +render -noreset
+sleep 3
+export DISPLAY=:99
+
+# Run extra commands
+exec "$@"
